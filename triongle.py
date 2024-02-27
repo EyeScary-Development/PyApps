63c3753cabb8e01f5cfcee43618f67c2d60b68c3
Pittab <120305267+Pittab@users.noreply.github.com>
2024-02-27 17:35:33 +0000

    Added some adapted apps rom Python-Utilities

---
+++ triongle.py
@@ -0,0 +1,5 @@
+x= float(input("input the width of the triangle "))
+y= float(input("input the height of the triangle "))
+result= (x*y)/2
+print("the area of your triangle is", result, "units squared")
+
---
+++ volcalc.py
@@ -0,0 +1,6 @@
+w=float(input("input the width of your box "))
+h=float(input("input the height of your box "))
+d=float(input("input the depth of your box "))
+
+print("your box has a volume of", w*h*d, "units squared")
+
