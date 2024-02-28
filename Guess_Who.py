c4aba2487206317aeff95659fc16b17a49fff333
thebookcheese <160770461+thebookcheese@users.noreply.github.com>
2024-02-28 12:48:15 +0000

    Update Guess_Who.py

--- Guess_Who.py
+++ Guess_Who.py
@@ -1,12 +1,13 @@
 Paul = {
   "Age": "Young",
   "Hair": "Brown",
+  "hair": "brown",
   "Eyes": "Blue",
   "Gender": "Male"
 }
 
 Paul_Hair = input("What is Paul's hair colour")
-if Paul_Hair in Paul["Hair"]:
+if Paul_Hair in Paul["Hair"] or Paul_Hair in Paul["hair"]:
   print("Correct")
 else:
   print("False")
