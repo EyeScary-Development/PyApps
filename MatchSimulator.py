4472a800de7e5e58726e070f0d9b3e7032cf4395
thebookcheese <160770461+thebookcheese@users.noreply.github.com>
2024-02-29 20:35:52 +0000

    Code Fixed

--- MatchSimulator.py
+++ MatchSimulator.py
@@ -1,3 +1,5 @@
+import random
+
 PremList = {
   "1": 84,
   "2": 76,
@@ -17,8 +19,8 @@ PremName = {
 Lower_Range = 45
 Upper_Range = 55
 print("What 2 teams do you want to simulate a match between? Type the number that is in brackets beside the team")
-Team1 = input("Arsenal (1), Aston Villa (2), Bournemouth (3), Brentford (4), Brighton (5), Burnley (6)"))
-Team2 = input("Arsenal (1), Aston Villa (2), Bournemouth (3), Brentford (4), Brighton (5), Burnley (6)"))
+Team1 = input("Arsenal (1), Aston Villa (2), Bournemouth (3), Brentford (4), Brighton (5), Burnley (6)")
+Team2 = input("Arsenal (1), Aston Villa (2), Bournemouth (3), Brentford (4), Brighton (5), Burnley (6)")
 if Team1 == Team2:
   print("No, do not pick 2 teams of the same")
   exit()
@@ -27,7 +29,7 @@ else:
 
 if Team1 in PremList and Team2 in PremList:
   if PremList[Team1] > PremList[Team2]:
-    Thing = random.random.randint(1,100)
+    Thing = random.randint(1,100)
     if Thing > PremList[Team2]:
       print(PremName[Team2], " won")
     elif Thing < PremList[Team1] and Thing > PremList[Team2]:
@@ -37,7 +39,7 @@ if Team1 in PremList and Team2 in PremList:
     else:
       print("Broken Code")
   else:
-      Thing = random.random.randint(1,100)
+    Thing = random.random.randint(1,100)
     if Thing > PremList[Team2]:
       print(PremName[Team2], " won")
     elif Thing > PremList[Team1] and Thing < PremList[Team2]:
